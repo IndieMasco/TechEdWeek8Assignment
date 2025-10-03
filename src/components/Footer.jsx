e0e2d79cefb6331b307a685fb70fc0267c965cd1
sam <s.clarkyy.1994@gmail.com>
2025-10-03 16:50:44 +0100

    footer is done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <>
       <div className="footer-container">
-        <p className="footer-text">Made by S P J Clark</p>
         <div className="footer-links">
+          <p>Made by S P J Clark </p> <br />
           <a href="https://github.com/IndieMasco">GitHub</a>
           <a href="https://www.linkedin.com/in/sam-p-j-clark/">Linkedin</a>
         </div>
